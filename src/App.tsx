ef81adafd2d3efbb96143e2ef2f0047399aba73a
Ayush <ayush.tiwari@codenicely.in>
2024-09-13 13:39:32 +0530

    shadcn config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Button } from "./components/ui/button"
 
 function App() {
 
   return (
     <div className="">
       <div className="text-4xl text-red-600">Hello</div>
+      <div className="">
+        <Button variant="destructive">Click me</Button>
+      </div>
     </div>
   )
 }
