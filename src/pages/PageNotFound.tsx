9f48a677a147af95043bc6364d90c6c12e7d58cd
Ayush <ayushkumar9601@gmail.com>
2024-09-24 15:17:42 +0530

    loading and pagenotfound done

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -2,7 +2,7 @@
 export const PageNotFound = () => {
   return (
     <div className="w-full h-screen flex justify-center items-center bg-slate-500">
-      <h1 className="text-7xl">Page Not Found</h1>
+      <h1 className="text-7xl font-bold text-slate-300">Page Not Found</h1>
     </div>
   )
 }
--- src/utils/Loading.tsx
+++ src/utils/Loading.tsx
@@ -1,8 +1,8 @@
 
 export const Loading = () => {
     return (
-        <div className="">
-            <h1 className="text-5xl">Loading...</h1>
+        <div className="flex items-center justify-center h-screen bg-gray-100">
+            <div className="w-16 h-16 border-8 border-t-8 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
         </div>
-    )
+    );
 }
